5fcf150f4b8af5982840ee6aebf64a81aa607431
Colin Vieson <cvieson1@umbc.edu>
2021-05-16 21:27:59 -0400

    delete commented code

--- covid19-map/src/components/Covid19.jsx
+++ covid19-map/src/components/Covid19.jsx
@@ -84,35 +84,3 @@ const Covid19 = () => {
 };
 
 export default Covid19;
-
-/*
-class Covid19 extends Component {
-  state = {
-    counties: [],
-  };
-
-  loadcountyTask = new LoadcountyTask();
-
-  componentDidMount() {
-    this.loadcountyTask.load((counties) => this.setState({ counties }));
-  }
-
-  render() {
-    const { counties } = this.state;
-    return (
-      <div>
-        {counties.length === 0 ? (
-          <Loading />
-        ) : (
-          <div>
-            <CovidMap counties={counties} />
-            <Legend legendItems={legendItems} />
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-export default Covid19;
-*/
